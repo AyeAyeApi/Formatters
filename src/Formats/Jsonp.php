69a840f4057a98d581af20a4dc0f6e3a030738a4
Daniel Mason <daniel@loftdigital.com>
2014-08-20 01:03:25 +0100

    Improving Jsonp

--- src/Formats/Jsonp.php
+++ src/Formats/Jsonp.php
@@ -12,12 +12,31 @@ use Gisleburt\Formatter\Format;
 
 class Jsonp extends Format {
 
+    protected $callbackName;
+
+    public function __construct($callbackName = null) {
+        $this->setCallbackName($callbackName);
+    }
+
+    /**
+     * Set the name of the javascript function that will be called
+     * @param $callbackName
+     */
+    public function setCallbackName($callbackName) {
+        $this->callbackName = $callbackName;
+    }
+
     public function sendHeaders() {
-        header('Content-Type: application/json');
+        header('Content-Type: application/javascript');
     }
 
     public function format($data, $name = null) {
-        return json_encode($data);
+        $callbackName = $this->callbackName;
+        if(!$callbackName) {
+            $callbackName = $name ? $name : 'callback';
+        }
+        $json = json_encode($data);
+        return "$callbackName($json);";
     }
 
 } 
