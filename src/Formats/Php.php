e6d64cd829a970a75212a57de4793f84df5c4b49
Daniel Mason <daniel@loftdigital.com>
2014-08-20 10:08:39 +0100

    Expanded the Php Formatter to allow you to include an actual php file that will call a callback. Needs more testing.

--- src/Formats/Php.php
+++ src/Formats/Php.php
@@ -12,12 +12,37 @@ use Gisleburt\Formatter\Format;
 
 class Php extends Format {
 
+    protected $callbackName;
+
+    public function __construct($callbackName = null) {
+        $this->setCallbackName($callbackName);
+    }
+
+    /**
+     * Set the name of the javascript function that will be called
+     * @param $callbackName
+     */
+    public function setCallbackName($callbackName) {
+        $this->callbackName = $callbackName;
+    }
+
     public function sendHeaders() {
-        header('Content-Type: application/json');
+        header('Content-Type: application/php');
+    }
+
+    public function getHeader() {
+        if($this->callbackName) {
+            return '<?php';
+        }
+        return parent::getHeader();
     }
 
     public function format($data, $name = null) {
-        return serialize($data);
+        $serialisedData = serialize($data);
+        if($this->callbackName) {
+            $serialisedData = $this->callbackName."(unserialize($serialisedData));";
+        }
+        return $serialisedData;
     }
 
 } 
