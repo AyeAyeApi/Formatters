2702ad710ede78da20cb14cbc0c645e4e1751956
Daniel Mason <daniel@danielmason.com>
2015-08-23 23:02:24 +0000

    Json Formatter Test

--- tests/Formats/JsonFormatterTest.php
+++ tests/Formats/JsonFormatterTest.php
@@ -10,58 +10,18 @@ namespace AyeAye\Formatter\Tests\Formats;
 use AyeAye\Formatter\Formats\Json;
 use AyeAye\Formatter\Tests\TestCase;
 
+/**
+ * Class JsonFormatterTest
+ * @package AyeAye\Formatter\Tests
+ * @coversDefaultClass \AyeAye\Formatter\Formats\Json
+ */
 class JsonFormatterTest extends TestCase
 {
-
-    public function testContentType()
-    {
-        $jsonFormatter = new Json();
-        $contentType = $jsonFormatter->getContentType();
-        $this->assertTrue(
-            $contentType === 'application/json',
-            'Incorrect content type for Json'
-        );
-    }
-
-    public function testHeader()
-    {
-        $jsonFormatter = new Json();
-        $this->assertTrue(
-            $jsonFormatter->getHeader() === '',
-            'Json header was not an empty string'
-        );
-    }
-
-    public function testFooter()
-    {
-        $jsonFormatter = new Json();
-        $this->assertTrue(
-            $jsonFormatter->getFooter() === '',
-            'Json footer was not an empty string'
-        );
-    }
-
-    public function testSimpleObjectJson()
-    {
-        $blankObject = new \stdClass();
-        $jsonFormatter = new Json();
-        $this->assertTrue(
-            $jsonFormatter->format($blankObject) === '{}',
-            'Json did not contain an empty object'
-        );
-    }
-
-    public function testSimpleArrayJson()
-    {
-        $blankArray = [];
-        $jsonFormatter = new Json();
-        $this->assertTrue(
-            $jsonFormatter->format($blankArray) === '[]',
-            'Json did not contain an empty array'
-        );
-    }
-
-    public function testComplexObject()
+    /**
+     * @test
+     * @covers ::format
+     */
+    public function testFormat()
     {
         $complexObject = (object)[
             'childObject' => (object)[
@@ -72,12 +32,11 @@ class JsonFormatterTest extends TestCase
                 'element2'
             ]
         ];
-        $expectedJson = '{"childObject":{"property":"value"},"childArray":["element1","element2"]}';
         $jsonFormatter = new Json();
-        $json = $jsonFormatter->format($complexObject);
-        $this->assertTrue(
-            $json === $expectedJson,
-            'Json did not contain an complex object'
+        $expectedJson = '{"childObject":{"property":"value"},"childArray":["element1","element2"]}';
+        $this->assertJsonStringEqualsJsonString(
+            $expectedJson,
+            $jsonFormatter->format($complexObject)
         );
     }
 }
