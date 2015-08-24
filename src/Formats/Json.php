252552b65ef899765b53e44b1f83fceaba347342
Daniel Mason <daniel@danielmason.com>
2015-08-24 07:26:55 +0000

    Added comments to Json

--- src/Formats/Json.php
+++ src/Formats/Json.php
@@ -9,11 +9,25 @@ namespace AyeAye\Formatter\Formats;
 
 use AyeAye\Formatter\Formatter;
 
+/**
+ * Class Json
+ * @package AyeAye\Formatter\Formats
+ */
 class Json extends Formatter
 {
 
+    /**
+     * Override content type
+     * @var string
+     */
     protected $contentType = 'application/json';
 
+    /**
+     * Format part of the data
+     * @param mixed $data
+     * @param string|null $name
+     * @return string
+     */
     public function format($data, $name = null)
     {
         return json_encode($data);
