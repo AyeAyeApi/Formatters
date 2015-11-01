ef5c0f2959e46dfa4d6ce01b45c5741b345acbbf
Daniel Mason <daniel@danielmason.com>
2015-11-01 17:03:47 +0000

    Reader factory

---
+++ src/ReaderFactory.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Reader.php
+ * @author    Daniel Mason <daniel@danielmason.com>
+ * @copyright 2015 Daniel Mason
+ * @license   GPL 3
+ * @see       https://github.com/AyeAyeApi/Formatters
+ */
+
+namespace AyeAye\Formatter;
+
+
+/**
+ * Interface Reader
+ * Read a formatted string and turn it back into data
+ * @package AyeAye/Formatters
+ * @see     https://github.com/AyeAyeApi/Formatters
+ */
+class ReaderFactory implements Reader
+{
+
+    /**
+     * @var Reader[]
+     */
+    protected $readers = [];
+
+    /**
+     * @param Reader[] $readers
+     */
+    public function __construct(array $readers = [])
+    {
+        foreach($readers as $reader) {
+            $this->addReader($reader);
+        }
+    }
+
+    /**
+     * @param Reader $reader
+     * @return $this
+     */
+    public function addReader(Reader $reader)
+    {
+        $this->readers[] = $reader;
+        return $this;
+    }
+
+    /**
+     * @param $string
+     * @return array
+     */
+    public function read($string)
+    {
+        foreach($this->readers as $reader) {
+            $result = $reader->read($string);
+            if($result) {
+                return $result;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ tests/ReaderFactoryTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * ReaderFactoryTest.php
+ * @author    Daniel Mason <daniel@danielmason.com>
+ * @copyright 2015 Daniel Mason
+ * @license   GPL 3
+ * @see       https://github.com/AyeAyeApi/Formatters
+ */
+
+namespace AyeAye\Formatter\Tests;
+
+use AyeAye\Formatter\Reader;
+use AyeAye\Formatter\Reader\Json;
+use AyeAye\Formatter\Reader\QueryString;
+use AyeAye\Formatter\Reader\Xml;
+use AyeAye\Formatter\ReaderFactory;
+
+/**
+ * Class ReaderFactoryTest
+ * Test the reader factory
+ * @package AyeAye/Formatters
+ * @see     https://github.com/AyeAyeApi/Formatters
+ * @coversDefaultClass \AyeAye\Formatter\ReaderFactory
+ */
+class ReaderFactoryTest extends TestCase
+{
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @uses \AyeAye\Formatter\ReaderFactory::addReader
+     */
+    public function testConstruct()
+    {
+        $json = new Json();
+        $xml = new Xml();
+        $query = new QueryString();
+
+        $readerFactory = new ReaderFactory([
+            $json,
+            $xml,
+        ]);
+
+        $readers = $this->getObjectAttribute($readerFactory, 'readers');
+
+        $this->assertContains(
+            $json,
+            $readers
+        );
+        $this->assertContains(
+            $xml,
+            $readers
+        );
+        $this->assertNotContains(
+            $query,
+            $readers
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @uses \AyeAye\Formatter\ReaderFactory::addReader
+     * @expectedException \PHPUnit_Framework_Error
+     */
+    public function testConstructFail()
+    {
+        new ReaderFactory([new \stdClass()]);
+    }
+
+    /**
+     * @test
+     * @covers ::addReader
+     * @uses \AyeAye\Formatter\ReaderFactory::__construct
+     */
+    public function testAddReader()
+    {
+        $json = new Json();
+        $readerFactory = new ReaderFactory();
+        $readers = $this->getObjectAttribute($readerFactory, 'readers');
+
+        $this->assertNotContains(
+            $json,
+            $readers
+        );
+
+        $readerFactory->addReader($json);
+        $readers = $this->getObjectAttribute($readerFactory, 'readers');
+
+        $this->assertContains(
+            $json,
+            $readers
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::addReader
+     * @uses \AyeAye\Formatter\ReaderFactory::__construct
+     * @expectedException \PHPUnit_Framework_Error
+     */
+    public function testAddReaderFail()
+    {
+        $readerFactory = new ReaderFactory();
+        $readerFactory->addReader(new \stdClass());
+    }
+
+    /**
+     * @test
+     * @covers ::read
+     * @uses \AyeAye\Formatter\ReaderFactory::__construct
+     * @uses \AyeAye\Formatter\ReaderFactory::addReader
+     */
+    public function testRead()
+    {
+        $testString = 'testString';
+        $testResult = 'testResult';
+        /** @var \PHPUnit_Framework_MockObject_MockObject|Reader $reader */
+        $reader = $this->getMockForAbstractClass('\AyeAye\Formatter\Reader');
+        $reader
+            ->expects($this->once())
+            ->method('read')
+            ->with($testString)
+            ->will($this->returnValue($testResult));
+
+        $readerFactory = new ReaderFactory();
+
+        $this->assertNull(
+            $readerFactory->read($testString)
+        );
+
+        $readerFactory->addReader($reader);
+
+        $this->assertSame(
+            $testResult,
+            $readerFactory->read($testString)
+        );
+    }
+
+}
